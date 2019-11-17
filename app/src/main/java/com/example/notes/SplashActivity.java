30fbf78ff7321eced3487236ce85097058265f4c
xtatick <brendasanchez9310@gmail.com>
2019-11-16 23:21:31 -0300

    second commit

---
+++ app/src/main/java/com/example/notes/SplashActivity.java
@@ -0,0 +1,41 @@
+package com.example.notes;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.antivity_splash);
+
+        runSplash();
+    }
+
+    private void runSplash() {
+        final Runnable splash = new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(splash);
+            }
+        };
+
+        Timer timer = new Timer();
+        timer.schedule(task, 2000);
+    }
+}

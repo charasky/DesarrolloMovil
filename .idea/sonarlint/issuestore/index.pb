a665d3c6f268c9a404018a7556ebbc9516e83146
barrio31 <brendasanchez9310@gmail.com>
2019-12-04 17:01:21 -0300

    arreglo layout reporte

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+g
+7app/src/main/java/org/lapoderosa/app/LoginActivity.java,6/9/69a94213709849a04cde925f4e753ff5a42241fb
--- app/src/main/java/org/lapoderosa/app/ReporteActivity.java
+++ app/src/main/java/org/lapoderosa/app/ReporteActivity.java
@@ -1,6 +1,7 @@
 package org.lapoderosa.app;
 
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
@@ -20,16 +21,18 @@ public class ReporteActivity extends AppCompatActivity {
 
     private static final String TAG = "ReporteActivity";
 
-    private TextView tvDate;
+    private TextView tvDate1,tvDate2,tvHora1;
     private DatePickerDialog.OnDateSetListener mDateSetListener;
+    private TimePickerDialog.OnTimeSetListener timeSetListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reporte);
-        tvDate = findViewById(R.id.tvDate);
+        tvDate1 = findViewById(R.id.tvDateEntrevista);
+        tvDate2 = findViewById(R.id.tvDateHecho);
 
-        tvDate.setOnClickListener(new View.OnClickListener() {
+        tvDate1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Calendar cal = Calendar.getInstance();
@@ -54,9 +57,44 @@ public class ReporteActivity extends AppCompatActivity {
                 Log.d(TAG, "onDateSet: mm/dd/yyyy: " + month + "/" + dayOfMonth + "/" + year);
 
                 String date = month + "/" + dayOfMonth + "/" + year;
-                tvDate.setText(date);
+                tvDate1.setText(date);
             }
         };
 
+        tvDate2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(
+                        ReporteActivity.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        mDateSetListener,
+                        year, month, day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                month += 1;
+                Log.d(TAG, "onDateSet: mm/dd/yyyy: " + month + "/" + dayOfMonth + "/" + year);
+
+                String date = month + "/" + dayOfMonth + "/" + year;
+                tvDate2.setText(date);
+            }
+        };
+
+        /*tvHora1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Time time = new Time();
+            }
+        });*/
     }
 }

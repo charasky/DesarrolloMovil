96e8ddeadfa39e93eec54ce9536070aab55d9b46
barrio31 <brendasanchez9310@gmail.com>
2019-12-09 22:10:30 -0300

    cosas del topo :v

--- app/src/main/java/org/lapoderosa/app/ReporteActivity.java
+++ app/src/main/java/org/lapoderosa/app/ReporteActivity.java
@@ -9,23 +9,36 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.lapoderosa.app.R;
 
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ReporteActivity extends AppCompatActivity {
 
-    Button btnGuardar;
+
     private static final String TAG = "ReporteActivity";
 
     private TextView tvDate1,tvDate2,tvHora1;
-    private DatePickerDialog.OnDateSetListener mDateSetListener;
+    private DatePickerDialog.OnDateSetListener m1DateSetListener;
+    private DatePickerDialog.OnDateSetListener m2DateSetListener;
     private TimePickerDialog.OnTimeSetListener timeSetListener;
-
+    private Button guardar;
+    private EditText edtNombre,edtApellido,edtAsamblea;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +46,10 @@ public class ReporteActivity extends AppCompatActivity {
         setContentView(R.layout.activity_reporte);
         tvDate1 = findViewById(R.id.tvDateEntrevista);
         tvDate2 = findViewById(R.id.tvDateHecho);
+        guardar = findViewById(R.id.btnGuardar);
+        edtNombre = findViewById(R.id.edtNombreEntrevistador);
+        edtApellido = findViewById(R.id.edtApellidoEntrevistador);
+        edtAsamblea = findViewById(R.id.edtAsamblea);
 
         tvDate1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,14 +62,14 @@ public class ReporteActivity extends AppCompatActivity {
                 DatePickerDialog dialog = new DatePickerDialog(
                         ReporteActivity.this,
                         android.R.style.Theme_Holo_Light_Dialog_MinWidth,
-                        mDateSetListener,
+                        m1DateSetListener,
                         year, month, day);
                 dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
                 dialog.show();
             }
         });
 
-        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+        m1DateSetListener = new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
                 month += 1;
@@ -74,14 +91,14 @@ public class ReporteActivity extends AppCompatActivity {
                 DatePickerDialog dialog = new DatePickerDialog(
                         ReporteActivity.this,
                         android.R.style.Theme_Holo_Light_Dialog_MinWidth,
-                        mDateSetListener,
+                        m2DateSetListener,
                         year, month, day);
                 dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
                 dialog.show();
             }
         });
 
-        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+        m2DateSetListener = new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
                 month += 1;
@@ -92,11 +109,49 @@ public class ReporteActivity extends AppCompatActivity {
             }
         };
 
+        guardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ejecutarServicio("http://ec2-3-136-55-99.us-east-2.compute.amazonaws.com/proyecto/insertar_datos_entrevistador.php");
+            }
+        });
+
         /*tvHora1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Time time = new Time();
             }
         });*/
+
+    }
+
+    private void ejecutarServicio(String URL) {
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Toast.makeText(getApplicationContext(),"Operacion Exitosa",Toast.LENGTH_SHORT).show();
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                Toast.makeText(getApplicationContext(), error.toString(), Toast.LENGTH_SHORT).show();
+
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> parametros = new HashMap<String,String>();
+                parametros.put("usu_nombre",edtNombre.getText().toString());
+                parametros.put("usu_apellido",edtApellido.getText().toString());
+                parametros.put("usu_asamblea",edtAsamblea.getText().toString());
+                parametros.put("usu_fecha",tvDate1.getText().toString());
+                return parametros;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
     }
 }
